4ed5ba8912d8b436a6e5c20df0c235d46d98fcdb
zaiyong <yzylin0@gmail.com>
2020-02-15 23:06:36 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get(`/`, (req, res, next) => {
-    res.send(`Hello Deploy auto`).end()
+    res.send(`Hello Deploy auto updated second Version`).end()
 })
 
 app.listen(9471, () => { console.log(`app is running at port: 9038`)})
