3fb6278c065d8e6a2d37a7e9ff41028b536b2ff1
zaiyong <yzylin0@gmail.com>
2020-03-18 14:31:33 +0800

    update port

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get(`/`, (req, res, next) => {
     res.send(`Hello Deploy auto updated second Version`).end()
 })
 
-app.listen(9471, () => { console.log(`app is running at port: 9038`)})
+app.listen(9232, () => { console.log(`app is running at port: 9232`)})
